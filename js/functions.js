50d66b015facc39681a89b087b2cd6ab4c2478cd
Rodrigo Rimbau <rodrigorimbau94@gmail.com>
2022-01-28 04:14:44 +0100

    Proyect Final

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+function modalVideo(){
+    let modal = document.getElementById("modal-Conta").style;
+    if (modal.display == "none"){
+        modal.display="flex";
+    }else{
+        modal.display="none"
+    }
+}
